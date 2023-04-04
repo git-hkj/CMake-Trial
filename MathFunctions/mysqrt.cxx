ecb6799da8fb20f12e7a63d3e1edbab8e9996620
git-hkj <hkj1994@live.com>
2023-04-04 16:01:29 +0200

    Step 8
    clean code

--- MathFunctions/mysqrt.cxx
+++ MathFunctions/mysqrt.cxx
@@ -1,5 +1,5 @@
 #include <iostream>
-#include <cmath> // 7-4
+
 #include "MathFunctions.h"
 
 // a hack square root calculation using simple operations
@@ -10,14 +10,9 @@ double mysqrt(double x)
     return 0;
   }
 
-//7-5: If log and exp are available on the system, then use them to compute the square root in the mysqrt function
-#if defined(HAVE_LOG) && defined(HAVE_EXP)
-  double result = std::exp(std::log(x) * 0.5);
-  std::cout << "Computing sqrt of " << x << " to be " << result
-      << " using log and exp" << std::endl;
-#else
+
   double result = x;
-#endif
+
 
   // do ten iterations
   for (int i = 0; i < 10; ++i) 
